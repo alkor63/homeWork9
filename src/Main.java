ae21a033e8c1725e54e84a2d517ccc80d5402d07
alkor63 <alkor@medi-spa.ru>
2022-11-17 13:22:17 +0300

    hw9_2 - Methods

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,41 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void leapYear(int yearNum) {
     double yearDiv4 = yearNum / 4.0;
-        if(yearDiv4 %1!=0.0)System.out.println(yearNum +"год не високосный");
+            if(yearDiv4 %1!=0.0)System.out.println(yearNum +" - не високосный год");
         else if(yearDiv4 %25==0.0&&yearDiv4 %100!=0.0)System.out.println(yearNum +" - не високосный год");
         else System.out.println(yearNum +" - високосный год");
-}
-    public static void main(String[] args) {
+        }
+    public static void clientDevice(boolean clientOS, int clientDeviceYear) {
+        String lightVersion;
+        if (clientDeviceYear >= 2015) lightVersion = " ";
+        else lightVersion = " облегченную ";
+        if (clientOS) System.out.println("Установите"+lightVersion+"версию приложения для Android по ссылке");
+        else System.out.println("Установите"+lightVersion+"версию приложения для iOS по ссылке");
+    }
+        public static void main(String[] args) {
 
         System.out.println(" * Task 10-1 ");
         int yearNum = 2100;
         leapYear(yearNum);
         yearNum = 2000;
         leapYear(yearNum);
-
+        int currentYear = LocalDate.now().getYear();
+        System.out.print("Текущий год ");
+        leapYear(currentYear);
+// Task 2
+        System.out.println("\n * Task 2 ");
 
         boolean clientOS = true;
-        if (clientOS) System.out.println("Установите версию приложения для Android по ссылке");
-        else System.out.println("Установите версию приложения для iOS по ссылке");
-
-// Task 2
-        System.out.println(" * Task 2 ");
-        int clientDeviceYear;
-        clientDeviceYear = 2014;
-        String lightVersion;
-        if (clientDeviceYear >= 2015) lightVersion = " ";
-        else lightVersion = " облегченную ";
-        if (clientOS) System.out.println("Установите"+lightVersion+"версию приложения для Android по ссылке");
-        else System.out.println("Установите"+lightVersion+"версию приложения для iOS по ссылке");
+            int clientDeviceYear = 2014;
+            System.out.println("Год выпуска мобилы клиента "+clientDeviceYear);
+            clientDevice(clientOS, clientDeviceYear);
+            clientOS = false;
+            clientDeviceYear = LocalDate.now().getYear();
+            System.out.println("Год выпуска мобилы клиента "+clientDeviceYear);
+            clientDevice(clientOS, clientDeviceYear);
 
 // Task 3
         System.out.println(" * Task 3 ");
